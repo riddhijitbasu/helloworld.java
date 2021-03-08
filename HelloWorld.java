6ff989556cd948a113ecf8ccdbebcfc52fb1ac0a
Riddhijit Basu <74175950+riddhijitbasu@users.noreply.github.com>
2021-03-08 22:34:10 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package hello.world; //this is the package meaning folders in movie genre
+public class HelloWorld //this the class
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello world!");
+        //this is object 
+    }   
+}
